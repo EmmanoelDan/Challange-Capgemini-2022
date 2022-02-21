8de4482a96679db7f367ffcd66a945766ba6308a
EmmanoelDan <emmanuel2011.07@gmail.com>
2022-02-20 21:15:07 -0300

    Terceira Questao

--- src/com/company/challange3/AnagramsValidation.java
+++ src/com/company/challange3/AnagramsValidation.java
@@ -1,5 +1,38 @@
 package com.company.challange3;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class AnagramsValidation {
+    // Approach 1 - Using Arrays
+    public static boolean isAnagram(String str1, String str2) {
+        if (str1.length() != str2.length())
+            return false;
+        else {
+            char[] str1Array = str1.toCharArray();
+            char[] str2Array = str2.toCharArray();
 
+            Arrays.sort(str1Array);
+            Arrays.sort(str2Array);
+
+            return Arrays.equals(str1Array, str2Array);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\nDigite a primeira palavra: ");
+        String str1 = sc.nextLine();
+
+        System.out.println("Digite a segunda palavra: ");
+        String str2 = sc.nextLine();
+
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        if(isAnagram(str1, str2))
+            System.out.println("As palavras: " + str1 +" e " + str2 + " são Anagramas");
+        else
+            System.out.println("As palavras: " + str1 +" e " + str2 + " não são Anagramas");
+    }
 }
